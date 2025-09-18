f4e40860c263491d716fb959333121645b8ac990
Eric Hoover <ehoover3@yahoo.com>
2025-09-18 10:25:51 -0400

    modify env variable names

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
-console.log("EBAY_CLIENT_ID at build time:", process.env.EBAY_CLIENT_ID);
-console.log("EBAY_CLIENT_SECRET at build time:", process.env.EBAY_CLIENT_SECRET);
-console.log("EBAY_DEV_ID at build time:", process.env.EBAY_DEV_ID);
+console.log("EBAY_CLIENT_ID at build time:", process.env.NEXT_PUBLIC_EBAY_CLIENT_ID);
+console.log("EBAY_CLIENT_SECRET at build time:", process.env.NEXT_PUBLIC_EBAY_CLIENT_SECRET);
+console.log("EBAY_DEV_ID at build time:", process.env.NEXT_PUBLIC_EBAY_DEV_ID);
 
 const nextConfig: NextConfig = {
   /* config options here */
